ec68d90b0beae8e83d17034e0d7a99725710c8ae
xiaowu.zhu <xiaowu.zhu@daocloud.io>
2023-03-08 11:04:43 +0800

    update

--- src/test/java/io/kubesphere/devops/HelloWorldControllerFailedTest.java
+++ src/test/java/io/kubesphere/devops/HelloWorldControllerFailedTest.java
@@ -6,14 +6,6 @@ import static org.junit.Assert.assertEquals;
 
 public class HelloWorldControllerFailedTest {
 
-    @Test
-    public void testSayHelloFailed() {
-        assertEquals("Really appreciate your star, that's the power of our life.","dasdadadada");
-    }
 
-     @Test
-    public void testSayHelloFailed2() {
-        assertEquals("Really appreciate your star, that's the power of our life.", "dadadada");
-    }
 
 }
